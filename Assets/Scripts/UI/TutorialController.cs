a46c934beb6ec260182bae2e6d59da55c85238cf
Jan Wójcik <jediorder3@gmail.com>
2022-10-03 16:42:09 +0200

    Tutorial & menus wip

---
+++ Assets/Scripts/UI/TutorialController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour
+{
+    [SerializeField] GameObject previousButton;
+    [SerializeField] GameObject nextButton;
+    [SerializeField] List<GameObject> tutorialWindows;
+
+    int currentStep = 0;
+
+    public void NextStep()
+    {
+        if(previousButton.activeInHierarchy == false)
+            previousButton.SetActive(true);
+
+        if (currentStep == tutorialWindows.Count - 1)
+        {
+            nextButton.SetActive(false);
+        }
+
+        currentStep += 1;
+
+        tutorialWindows[currentStep - 1].SetActive(false);
+        tutorialWindows[currentStep].SetActive(true);
+    }
+
+    public void PreviousStep()
+    {
+        if (nextButton.activeInHierarchy == false)
+            nextButton.SetActive(true);
+
+        if (currentStep == 1)
+        {
+            previousButton.SetActive(false);
+        }
+
+        currentStep -= 1;
+
+        tutorialWindows[currentStep + 1].SetActive(false);
+        tutorialWindows[currentStep].SetActive(true);
+    }
+}
